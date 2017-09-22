9d9c8a94f729586bb4a8a0b2055433f2190b3253
Jorge Garcia <jorge.garcia@udc.es>
2017-09-22 18:08:07 +0200

    Added fragments cache

--- lib/thymeleaf/context/context_holder.rb
+++ lib/thymeleaf/context/context_holder.rb
@@ -31,4 +31,48 @@ class ContextHolder < Struct.new(:context, :parent_context)
     end
   end
   
+  def to_s
+    string='{'
+    self.context.to_h.each do |key,value|
+      if value.kind_of?(Array)
+        string+=to_s_child(value)
+      else
+        string+= (key.to_s)+':'+ (value.to_s)
+      end
+      string+=','  if !(self.context.to_h.to_a.last.last == value)
+    end
+    string+='}'
+  end
+  private 
+  def to_s_child(array)
+    string='['
+    array.each do |element|
+      if element.kind_of?(ContextStruct)
+        string+=to_s_child_context(element.to_h)
+      else
+        if element.kind_of?(Array)
+          string+=to_s_child(element) if !element.nil?
+        else
+          string+=element.to_s if !element.nil?
+        end
+      end
+      string+=',' if !(array.last==element)
+    end
+    string+=']'
+    string
+  end
+
+  def to_s_child_context(contextstruct)
+    string='{'
+    contextstruct.each do |key,value|
+      if value.kind_of?(Array)
+        string+=(key.to_s)+':'+ (to_s_child(value) if !value.nil?)
+      else
+        string+=(key.to_s) +':'+ (value.to_s)
+      end
+      string+= ',' if !(contextstruct.to_a.last.last == value)
+    end
+    string+='}'
+    string
+  end
 end
--- lib/thymeleaf/template_engine_sax.rb
+++ lib/thymeleaf/template_engine_sax.rb
@@ -3,8 +3,20 @@ module Thymeleaf
   	
   	def call(handler_nodes, context_holder)
   	  handler_nodes.each do |node|
-        if node.name != 'text-content'
-          process_node(context_holder,node,handler_nodes)
+        cache_key = get_cache_key(context_holder,node)#This operation is expensive in time
+        if Thymeleaf.configuration.cache_manager.fragment_cache.get(cache_key).nil?
+          filename = to_filename(cache_key,cache_key.length/10)#This operation is expensive in time
+          if node.name != 'text-content'
+            process_node(context_holder,node,handler_nodes)
+          end
+          Thymeleaf.configuration.cache_manager.fragment_cache.set(cache_key,node)
+          Thymeleaf.configuration.cache_manager.write_file_fragment(node,cache_key,filename)#TODO Do the write in an exit event
+          
+        else
+          new_node=Thymeleaf.configuration.cache_manager.fragment_cache.get(cache_key)
+          ind = handler_nodes.index(node)
+          handler_nodes.delete_at(ind)
+          handler_nodes.insert(ind,new_node)
         end
       end
       handler_nodes
@@ -13,16 +25,16 @@ module Thymeleaf
   	private
 
   	def process_node( context_holder, node,node_list)
-      #First we process the atributtes
-      attr_context=process_attributes(context_holder, node, node_list)
-      #Then we process the tags
-      children_context = process_tag(attr_context, node, node_list)
-      #Last we process the children nodes of the tree
-      node.children.each do|child|
-        if child.name != 'text-content'
-          process_node(children_context, child, node.children)
+        #First we process the atributtes
+        attr_context=process_attributes(context_holder, node, node_list)
+        #Then we process the tags
+        children_context = process_tag(attr_context, node, node_list)
+        #Last we process the children nodes of the tree
+        node.children.each do|child|
+          if child.name != 'text-content'
+            process_node(children_context, child, node.children)
+          end
         end
-      end
   	end
 
 	  def process_attributes(context_holder , node, node_list)
@@ -46,6 +58,7 @@ module Thymeleaf
 	  def process_tag(context_holder, node, node_list)
       dialects = Thymeleaf.configuration.dialects
       key, processor = * dialects.find_tag_processor(node.name)
+     
       process_element(context_holder, node, nil, key, processor, node_list)
 	  end
 
@@ -61,6 +74,43 @@ module Thymeleaf
 	  def processor_has_subcontext?(processor)
       processor.respond_to?(:has_subcontext?) && processor.has_subcontext?
 	  end
-  
+
+    def get_cache_key(context_holder, node)
+      context_string = context_holder.to_s
+      node_string = node.to_html
+      if node_string.nil?
+        cache_key=context_string
+      else
+        cache_key=context_string + node_string
+      end
+      cache_key
+    end
+     
+      
+    
+    def to_filename(cache_key,jump)
+      name=cache_key
+      len=name.length
+      if (jump!=0)
+        num=len/jump
+        i=0
+        str=''
+        while i<num+1
+          if (i*jump+(jump-1))<len
+            str1=name[i*jump..i*jump+(jump-1)]
+          else
+            str1=name[i*jump..len]
+          end
+          str+=str1.crypt("aa")
+          i+=1
+        end
+        str=str.gsub(/[\/.]/, '_')
+      else
+        str=name.crypt("aa")
+        str=str.gsub(/[\/.]/, '_')
+      end
+      str
+    end
+
   end
 end
