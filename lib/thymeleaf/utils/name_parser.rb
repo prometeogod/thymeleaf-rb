50fe2a9a28055d056190c4cb8227edb6368836f9
Jorge Garcia <jorge.garcia@udc.es>
2017-10-31 18:41:43 +0100

    Fixing bugs and renaming methods in default processor remove and name_parser

--- lib/thymeleaf/utils/name_parser.rb
+++ lib/thymeleaf/utils/name_parser.rb
@@ -1,5 +1,5 @@
 def to_cache_name(filename, suffix = nil)
-  cache_name = filename.gsub!(%r{[\/.]}, '_')
+  cache_name = filename.gsub(%r{[\/.]}, '_')
   cache_name += suffix unless suffix.nil?
   cache_name
 end
@@ -8,7 +8,7 @@ def to_file_name(cache_name, suffix = nil)
   cache_name_array = cache_name.split(suffix) unless suffix.nil?
   filename = cache_name_array[0]
   list = filename.rpartition('th')
-  filename_head = list[0].chop.gsub!(/[_]/, '/')
-  filename_tail = '.' + list[1] + list[2].gsub!(/[_]/, '.')
+  filename_head = list[0].chop.gsub(/[_]/, '/')
+  filename_tail = '.' + list[1] + list[2].gsub(/[_]/, '.')
   filename_head + filename_tail
 end
