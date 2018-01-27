28dac09480477c95514068a7e82b8283a02e4e2f
Jorge Garcia <jorge.garcia@udc.es>
2018-01-27 22:07:18 +0100

    Delete parsed template cache in Template

--- lib/thymeleaf/template.rb
+++ lib/thymeleaf/template.rb
@@ -1,10 +1,13 @@
+require_relative 'precompile/precompiler'
+require_relative 'precompile/writer'
+require_relative 'precompile/expresion'
+require_relative 'context/context_holder'
+require_relative './cache/node_value_date'
 require_relative 'processor'
 require_relative './utils/name_parser'
-# Thymeleaf module
+
+
 module Thymeleaf
-  require_relative 'context/context_holder'
-  require_relative './cache/node_value_date'
-  # Template class definition: defines the operations to render templates
   class Template < Struct.new(:template_markup, :context)
     def render(filename = nil)
       do_render template_markup, filename
@@ -20,30 +23,28 @@ module Thymeleaf
 
     private
 
-    def do_render(template, filename = nil)
-      if !precompiled?(template, context)
-        parsed_template = get_parsed_template(template, filename)
-        context_holder = ContextHolder.new(context)
-        TemplateEngine.new.call(parsed_template, context_holder)
-        procesed = to_rendered_string(parsed_template)
-        key = template + context.to_s
-        Thymeleaf.configuration.pre_cache.set(key,procesed)
-        procesed
-      else
-        key = template + context.to_s
-        Thymeleaf.configuration.pre_cache.get(key)
+    def do_render(template, filename=nil)
+      
+      dig = Digest::SHA1.hexdigest(template)
+      
+      precompiled_template = Thymeleaf.configuration.pre_cache.get(dig)
+      if !precompiled_template
+        handler = Thymeleaf::Parser.new(template).call
+        parsed_template = handler.nodes
+        precompiled_template = precompile(parsed_template) #Es un lambda
+        Thymeleaf.configuration.pre_cache.set(dig, precompiled_template)
       end
+      w = StringWriter.new
+      precompiled_template.call(context, w, Expr.new)
+      w.output
     end
 
-    def precompiled?(template, context)
-      key = template + context.to_s
-      template = Thymeleaf.configuration.pre_cache.get(key)
-      if template != nil 
-        true
-      else
-        false
-      end
+    def precompile(parsed_tree)
+      precompiled_template = Precompiler.new.precompile(parsed_tree) #Es un lambda
+      eval(precompiled_template)
     end
+
+    # Template file open
 
     def template_file_open(template_file)
       File.open template_file do |template|
@@ -52,73 +53,5 @@ module Thymeleaf
       end
     end
 
-    def to_rendered_string(parsed_template)
-      rendered = ''
-      parsed_template.each do |node|
-        if !node.to_html.nil?
-          rendered += node.to_html
-        else
-          rendered += "\n" # TODO, Beware this \n
-        end
-      end
-      rendered
-    end
-
-    def get_parsed_template(template, filename)
-      if !filename.nil?
-        key = to_cache_name(filename.dup, '.th.parsed_cache')
-        cache_manager = Thymeleaf.configuration.cache_manager
-        p_cache = cache_manager.p_cache
-        template_caching(p_cache, key, template, filename)
-      else
-        handler = Parser.new(template).call
-        handler.nodes
-      end
-    end
-
-    def template_caching(p_cache, key, template, filename)
-      if cached?(p_cache, key)
-        node_cached(p_cache, key, template, filename)
-      else
-        node_uncached(p_cache, key, template, filename)
-      end
-    end
-
-    def node_cached(p_cache, key, template, filename)
-      nodevd = p_cache.get(key)
-      cache_date = nodevd.date
-      file_date = get_template_file_mtime(filename)
-      parsed_template = nodevd.value
-      date_comparison = cache_date <=> file_date
-      if date_comparison == -1
-        parsed_template = node_recached(p_cache, key, template)
-      end
-      parsed_template
-    end
-
-    def node_recached(p_cache, key, template)
-      p_cache.unset(key)
-      handler = Parser.new(template).call
-      parsed_template = handler.nodes
-      node = NodeValueDate.new(parsed_template)
-      p_cache.set(key, node)
-      parsed_template
-    end
-
-    def node_uncached(p_cache, key, template, _filename)
-      handler = Parser.new(template).call
-      parsed_template = handler.nodes
-      node = NodeValueDate.new(parsed_template)
-      p_cache.set(key, node)
-      parsed_template
-    end
-
-    def cached?(cache, key)
-      cache.set?(key)
-    end
-
-    def get_template_file_mtime(file)
-      File.mtime(file)
-    end
   end
 end
