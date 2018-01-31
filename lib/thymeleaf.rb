4ed4e5108cd745e400bd6075aa635a1581ac9345
Jorge Garcia <jorge.garcia@udc.es>
2018-01-31 17:42:40 +0100

    Added to the module thymeleaf the classes precompiler and template_key

--- lib/thymeleaf.rb
+++ lib/thymeleaf.rb
@@ -4,6 +4,8 @@ require_relative 'thymeleaf/parser'
 require_relative 'thymeleaf/processor'
 require_relative 'thymeleaf/template_engine'
 require_relative 'thymeleaf/template'
+require_relative 'thymeleaf/precompiler'
+require_relative 'thymeleaf/template_key'
 # Thymeleaf module
 module Thymeleaf
 end
