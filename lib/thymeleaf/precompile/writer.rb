fbcc5ab3732ca57e277f3624fb2790a041a31beb
Jorge Garcia <jorge.garcia@udc.es>
2018-01-31 17:44:56 +0100

    Modified writer purged subclass StringWriter

--- lib/thymeleaf/precompile/writer.rb
+++ lib/thymeleaf/precompile/writer.rb
@@ -1,34 +1,17 @@
 class Writer
-	def initialize(output)
-	  @buffer = []
-	  @output = output
-	end
+  def initialize
+    @buffer = []
+  end
 
-    def write(x)
-       @buffer << x
-    end
+  def write(x)
+    @buffer << x
+  end
     
-	def <<(x)
-		@buffer << x
-	end
-
-	def flush
-		@output.call(@buffer.join())
-		@buffer = []
-	end
-	def output
-	end
-end
-
-class StringWriter < Writer
-    def initialize
-      super(nil)
-    end
-
-	def flush
-	end
-
-	def output
-		@buffer.join
-	end
+  def <<(x)
+    @buffer << x
+  end
+	
+  def output
+    @buffer.join
+  end
 end
