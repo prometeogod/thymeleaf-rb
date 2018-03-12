f822316295d7f945bf72b0b25c5b16a224a20b44
Jorge Garcia <jorge.garcia@udc.es>
2018-03-12 17:31:06 +0100

    Removing obsolete clases and chunks of code

--- test/thymeleaf/utils/random_string_generator_test.rb
+++
@@ -1,8 +0,0 @@
-require 'thymeleaf/utils/random_string_generator'
-
-describe RandomStringGenerator do
-  it 'should generate a string' do
-    random = RandomStringGenerator.new.random
-    assert_equal random.is_a?(String), true
-  end
-end
